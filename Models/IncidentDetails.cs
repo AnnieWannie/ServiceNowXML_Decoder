7e437d6f6e6b0b622d6ef721d9fc2d15a82564dc
Anthony Elia <49228445+AnnieWannie@users.noreply.github.com>
2024-06-13 12:20:22 -0400

    Update Models

--- Models/IncidentDetails.cs
+++ Models/IncidentDetails.cs
@@ -16,6 +16,9 @@ namespace ServiceNowXMLToHTML.Models
         public string SysAttachmentDocument { get; set; }
         public string ChildIncident { get; set; }
         public string attachmentHash { get; set; }
+        public string assignmentGroup { get; set; }
         public Dictionary<string, byte[]> SysAttachmentByteData { get; set; }
+
+        public Dictionary<string, string> SysAttachmentType { get; set; }
     }
 }
